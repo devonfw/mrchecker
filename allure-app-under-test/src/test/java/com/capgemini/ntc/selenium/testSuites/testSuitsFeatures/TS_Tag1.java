3c348d5c7fa3311769110b7caec242c7674951db
Lukasz Stefaniszyn <stefaniszynlukasz@gmail.com>
2017-12-09 22:49:30 +0100

    clean up after structure rebuild

--- allure-app-under-test/src/test/java/com/capgemini/ntc/selenium/testSuites/testSuitsFeatures/TS_Tag1.java
+++ allure-app-under-test/src/test/java/com/capgemini/ntc/selenium/testSuites/testSuitsFeatures/TS_Tag1.java
@@ -1,22 +1,24 @@
 package com.capgemini.ntc.selenium.testSuites.testSuitsFeatures;
 
-import org.junit.experimental.categories.Categories.IncludeCategory;
 import org.junit.runner.RunWith;
 
 import com.capgemini.ntc.selenium.testSuites.testType.TestsTag1;
+import com.capgemini.ntc.test.core.utils.junitoolboxpi.WildcardPatternSuiteBF;
+import com.googlecode.junittoolbox.ExcludeCategories;
+import com.googlecode.junittoolbox.IncludeCategories;
 import com.googlecode.junittoolbox.SuiteClasses;
-import com.googlecode.junittoolbox.WildcardPatternSuite;
 
-@RunWith(WildcardPatternSuite.class)
-@SuiteClasses("**/*.class")
-@IncludeCategory(TestsTag1.class)
-public class TS_Tag1 {
-}
-
-// @RunWith(WildcardPatternSuiteBF.class)
-// @IncludeCategories({ TestsTag1.class })
-// @ExcludeCategories({})
-// @SuiteClasses({ "**/*.class" })
-// public class TS_Tag1 {
 //
-// }
+//@RunWith(WildcardPatternSuite.class)
+//@SuiteClasses("**/*.class")
+//@IncludeCategory(TestsTag1.class)
+//public class TS_Tag1 {
+//}
+
+@RunWith(WildcardPatternSuiteBF.class)
+@IncludeCategories({ TestsTag1.class })
+@ExcludeCategories({})
+@SuiteClasses({ "**/*.class" })
+public class TS_Tag1 {
+    
+}
