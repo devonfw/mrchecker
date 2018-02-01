6f90e425ce5d2ee664e97fe956d87b08a82bcce2
Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2018-02-01 16:00:25 +0100

    remove GoogleAnalytics code

--- allure-framework-modules/allure-core-module/src/main/java/com/capgemini/ntc/test/core/analytics/AnalyticsProvider.java
+++ allure-framework-modules/allure-core-module/src/main/java/com/capgemini/ntc/test/core/analytics/AnalyticsProvider.java
@@ -1,6 +1,5 @@
 package com.capgemini.ntc.test.core.analytics;
 
-import com.brsanthu.googleanalytics.GoogleAnalytics;
 import com.capgemini.ntc.test.core.logger.BFLogger;
 
 /**
@@ -10,43 +9,7 @@ import com.capgemini.ntc.test.core.logger.BFLogger;
  */
 public enum AnalyticsProvider implements IAnalytics {
 	
-	GOOGLE() {
-		private GoogleAnalytics instance;
-		
-		@Override
-		public void setInstance() {
-			String testTrackingID = "UA-110726320-1";
-			instance = GoogleAnalytics.builder()
-					.withTrackingId(testTrackingID)
-					.withAppName("Allure-Test-Framework")
-					.withAppVersion("1.0.0") // TASK: Get app version from pom.xml
-					.build();
-			
-		}
-		
-		public GoogleAnalytics getInstance() {
-			return instance;
-		}
-		
-		@Override
-		public void sendClassName(String packageName, String className, String description) {
-			
-			this.getInstance()
-					.pageView(packageName, className, description)
-					.postAsync();
-			
-		}
-		
-		@Override
-		public void sendMethodEvent(String analitycsCategoryName, String eventName) {
-			this.getInstance()
-					.event()
-					.eventCategory(analitycsCategoryName)
-					.eventAction(eventName)
-					.postAsync();
-		}
-		
-	},
+	
 	
 	DISABLED() {
 		@Override
