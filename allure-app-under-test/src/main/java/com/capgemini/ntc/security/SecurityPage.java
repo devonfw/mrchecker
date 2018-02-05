8e1a68fe7b632875771458ddf97e553e1e16dce1
Marek Puchalski <marek.puchalski@capgemini.com>
2018-02-05 22:13:10 +0100

    English fixed

--- allure-app-under-test/src/main/java/com/capgemini/ntc/security/SecurityPage.java
+++ allure-app-under-test/src/main/java/com/capgemini/ntc/security/SecurityPage.java
@@ -7,18 +7,17 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 /**
- * The class offers all security relevant tests common functionality that
- * can be shared among them like e.g. session management.
+ * The class offers security relevant functionality to be shared among test cases e.g. session management.
  *
  * @author Marek Puchalski, Capgemini
  */
 public abstract class SecurityPage extends BasePage {
 	
-	private ISessionManager sessionManager;
+	private static final Injector	injector	= Guice.createInjector(new SessionManagerModule());
+	private ISessionManager				sessionManager;
 	
 	public SecurityPage() {
 		super();
-		Injector injector = Guice.createInjector(new SessionManagerModule());
 		sessionManager = injector.getInstance(ISessionManager.class);
 	}
 	
