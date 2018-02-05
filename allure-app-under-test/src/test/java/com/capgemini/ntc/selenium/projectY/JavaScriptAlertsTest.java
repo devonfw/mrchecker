21cb21da8f935a37bd47bb98993f24cc2d67f9b6
Tomasz Supel <tomasz.supel@capgemini.com>
2018-02-05 17:08:10 +0100

    Done

--- allure-app-under-test/src/test/java/com/capgemini/ntc/selenium/projectY/JavaScriptAlertsTest.java
+++ allure-app-under-test/src/test/java/com/capgemini/ntc/selenium/projectY/JavaScriptAlertsTest.java
@@ -16,6 +16,7 @@ public class JavaScriptAlertsTest extends BaseTest {
 	
 	private TheInternetPage				theInternetPage;
 	private static JavaScriptAlertsPage	javaScriptAlertsPage;
+	private final String				randomString	= "random";
 	
 	@Override
 	public void setUp() {
@@ -27,7 +28,7 @@ public class JavaScriptAlertsTest extends BaseTest {
 	
 	@Override
 	public void tearDown() {
-		BFLogger.logInfo("Step5 - navigate back to The-Internet page");
+		BFLogger.logInfo("Step7 - navigate back to The-Internet page");
 		BasePage.navigateBack();
 	}
 	
@@ -38,29 +39,78 @@ public class JavaScriptAlertsTest extends BaseTest {
 		assertTrue("JavaScritp Alert page is not loaded", javaScriptAlertsPage.isLoaded());
 	}
 	
-	@Test
-	public void shouldOpenJSAlertBoxAndClickOkButtonTest() {
+	@Test // TC1
+	public void checkOkButtonOnBasicAlert() {
 		BFLogger.logInfo("Step 4: Click Alert Button");
 		javaScriptAlertsPage.clickAlertButton();
 		
+		BFLogger.logInfo("Step 5: Click OK Button on alert");
+		javaScriptAlertsPage.clickAlertAccept();
+		
+		BFLogger.logInfo("Step 6: Chcek the message");
 		assertEquals("Ok button inside Alert Box doesn't click",
 						"You successfuly clicked an alert",
 						javaScriptAlertsPage.readResultLabel());
-		
 	}
 	
-	@Test
-	public void validateJSConfirm() {
+	@Test // TC2
+	public void checkOkButtonOnConfirmAlert() {
 		BFLogger.logInfo("Step 4: Click Confirm Button");
 		javaScriptAlertsPage.clickConfirmButton();
 		
+		BFLogger.logInfo("Step 5: Click OK Button on alert");
+		javaScriptAlertsPage.clickAlertAccept();
+		
+		BFLogger.logInfo("Step 6: Chcek the message");
+		assertEquals("Ok button inside Confirm Box doesn't click",
+						"You clicked: Ok",
+						javaScriptAlertsPage.readResultLabel());
 	}
 	
-	@Test
-	public void validateJSPrompt() {
+	@Test // TC3
+	public void checkCancelButtonOnConfirmAlert() {
+		BFLogger.logInfo("Step 4: Click Confirm Button");
+		javaScriptAlertsPage.clickConfirmButton();
+		
+		BFLogger.logInfo("Step 5: Click Cancel Button on alert");
+		javaScriptAlertsPage.clickAlertDismiss();
+		
+		BFLogger.logInfo("Step 6: Chcek the message");
+		assertEquals("Cancel Button inside Confirm Box doesn't work",
+						"You clicked: Cancel",
+						javaScriptAlertsPage.readResultLabel());
+	}
+	
+	@Test // TC4
+	public void checkInputTextOnPromptAlert() {
+		
 		BFLogger.logInfo("Step 4: Click Prompt Button");
 		javaScriptAlertsPage.clickPromptButton();
 		
+		BFLogger.logInfo("Step 5: Input text on alert: " + randomString);
+		javaScriptAlertsPage.writeTextInAlert(randomString);
+		
+		BFLogger.logInfo("Step 6: Click OK Button on alert");
+		javaScriptAlertsPage.clickAlertAccept();
+		
+		BFLogger.logInfo("Step 7: Chcek the message");
+		assertEquals("The entered text doesn't match",
+						"You entered: " + randomString,
+						javaScriptAlertsPage.readResultLabel());
+	}
+	
+	@Test // TC5
+	public void checkCancelButtonOnPromptAlert() {
+		BFLogger.logInfo("Step 4: Click Prompt Button");
+		javaScriptAlertsPage.clickPromptButton();
+		
+		BFLogger.logInfo("Step 5: Click Cancel Button on alert");
+		javaScriptAlertsPage.clickAlertDismiss();
+		
+		BFLogger.logInfo("Step 6: Chcek the message");
+		assertEquals("Cancel Button inside Prompt Box doesn't work",
+						"You entered: null",
+						javaScriptAlertsPage.readResultLabel());
 	}
 	
 }
