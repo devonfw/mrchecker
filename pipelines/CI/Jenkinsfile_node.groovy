fe3cbaa5cd31d3c1cdcee5a798bce1a01079ab58
mchudzik <matchu183@wp.pl>
2018-02-18 17:44:15 +0100

    no message

--- pipelines/CI/Jenkinsfile_node.groovy
+++ pipelines/CI/Jenkinsfile_node.groovy
@@ -12,11 +12,11 @@ node(){
 	//	stashNotification("INPROGRESS");
 		utils.generateUserIDVariable(); //Generate USER_ID and USER_GROUP
         docker.image('teste2e:v1-0.0').inside("-u ${env.USER_ID}:${env.USER_GROUP}"){
-			withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${env.ARTIFACTORY_USER}", passwordVariable: 'ARTIFACTORY_PASSWORD', usernameVariable: 'ARTIFACTORY_USERNAME']]) {
+			//withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${env.ARTIFACTORY_USER}", passwordVariable: 'ARTIFACTORY_PASSWORD', usernameVariable: 'ARTIFACTORY_USERNAME']]) {
                 stageBuildCompile();    
                 // stageUnitTestsAndStaticAnalyze();   -- temporary commented
                 // stageIntegrationTests();			-- temporary commented
-            } 
+           // }
         }
 		
 		//stashNotification("SUCCESSFUL");
