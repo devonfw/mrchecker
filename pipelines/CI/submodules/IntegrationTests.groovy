0ab22458d70f7a7ef72090cac0518395d1f3ffed
mchudzik <matchu183@wp.pl>
2018-03-01 21:35:03 +0100

    added selenium grid for tests

--- pipelines/CI/submodules/IntegrationTests.groovy
+++ pipelines/CI/submodules/IntegrationTests.groovy
@@ -50,7 +50,7 @@ private generateAllureReport(){
 private runFirstTest() {
 	sh """
         cd ${env.PROJECT_HOME}
-        mvn clean test-compile test site -Dtest=RegisterOKTest
+        mvn clean test-compile test site -Dtest=RegisterOKTest -DcustomHubUrl=${env.HUBURL}
     """
 }
 
