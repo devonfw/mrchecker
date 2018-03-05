4f0dee3f8a4f0bbb173980acdb5e7a1920d1fdac
Tomasz Supel <tomasz.supel@capgemini.com>
2018-03-05 10:35:09 +0100

    Base page

--- allure-framework-modules/allure-webapi-module/src/test/java/com/capgemini/ntc/example/BasePageTest.java
+++ allure-framework-modules/allure-webapi-module/src/test/java/com/capgemini/ntc/example/BasePageTest.java
@@ -3,8 +3,19 @@ package com.capgemini.ntc.example;
 import org.junit.Test;
 
 import com.capgemini.ntc.example.core.BasePage;
+import com.capgemini.ntc.test.core.BaseTest;
 
-public class BasePageTest {
+public class BasePageTest extends BaseTest {
+	
+	@Override
+	public void setUp() {
+		
+	}
+	
+	@Override
+	public void tearDown() {
+		
+	}
 	
 	@Test
 	public void test() {
