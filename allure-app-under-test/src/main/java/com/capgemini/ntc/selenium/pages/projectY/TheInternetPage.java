4d7d76d48151290353207d1a8714b6ae2f29c297
krzysztofdrzazga <krzysztof.drzazga@capgemini.com>
2018-03-25 12:06:28 +0200

    Removed local version of chromedriver.exe to prevend merge conflict

--- allure-app-under-test/src/main/java/com/capgemini/ntc/selenium/pages/projectY/TheInternetPage.java
+++ allure-app-under-test/src/main/java/com/capgemini/ntc/selenium/pages/projectY/TheInternetPage.java
@@ -1,169 +1,168 @@
 package com.capgemini.ntc.selenium.pages.projectY;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-
 import com.capgemini.ntc.selenium.core.BasePage;
 import com.capgemini.ntc.selenium.core.newDrivers.elementType.Button;
 import com.capgemini.ntc.selenium.pages.environment.GetEnvironmentParam;
 import com.capgemini.ntc.test.core.logger.BFLogger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 
 public class TheInternetPage extends BasePage {
-	
-	private static final By	selectorCheckboxesLink			= By.cssSelector("li > a[href*=checkboxes]");
-	private static final By	selectorElementClick			= By.cssSelector("li > a[href*=abtest]");
-	private static final By	selectorBrokenImageClickLink	= By.cssSelector("li > a[href*=broken_images]");
-	private static final By	selectorDropdownClickLink		= By.cssSelector("li > a[href*=dropdown]");
-	private static final By	selectorMultipleWindowsLink		= By.cssSelector("li > a[href*=windows]");
-	private static final By	selectorBasicAuthLink			= By.cssSelector("li > a[href*=basic_auth]");
-	private static final By	selectorKeyPressesLink			= By.cssSelector("li > a[href*=key_presses]");
-	private static final By	selectorRedirectLink			= By.cssSelector("li > a[href*=redirector]");
-	private static final By	selectorJavaScriptAlertLink		= By.cssSelector("li > a[href*=javascript_alerts]");
-	private static final By	selectorHoversLink				= By.cssSelector("li > a[href*=hovers]");
-	private static final By	selectorSortableDataTablesLink	= By.cssSelector("li > a[href*=tables]");
-	private static final By	selectorChallengingDomClick		= By.cssSelector("li > a[href*=challenging_dom]");
-	private static final By	selectorStatusCodesLink			= By.cssSelector("li > a[href*=status_codes]");
-	private static final By	selectorDynamicContent			= By.cssSelector("li > a[href*=dynamic_content]");
-	private static final By	selectorHorizontalSliderLink	= By.cssSelector("li > a[href*=horizontal_slider]");
-	private static final By	selectorFormAuthenticationLink	= By.cssSelector("li > a[href*=login]");
-	private static final By	selectorForgotPasswordLink		= By.cssSelector("li > a[href*=forgot_password]");
-	private static final By	selectorExitIntentLink			= By.cssSelector("li > a[href*=exit_intent]");
-	private static final By	selectorDynamicLoadingLink		= By.cssSelector("li > a[href*=dynamic_loading]");
-	
-	@Override
-	public boolean isLoaded() {
-		BFLogger.logDebug("The internet page is loaded: " + getDriver().getCurrentUrl());
-		return getDriver().getCurrentUrl()
-						.equals(GetEnvironmentParam.THE_INTERNET_MAIN_PAGE.getValue());
-	}
-	
-	@Override
-	public void load() {
-		BFLogger.logDebug("load page");
-		getDriver().get(GetEnvironmentParam.THE_INTERNET_MAIN_PAGE.getValue());
-		getDriver().waitForPageLoaded();
-	}
-	
-	@Override
-	public String pageTitle() {
-		return getDriver().getTitle();
-	}
-	
-	public CheckboxesPage clickCheckboxesLink() {
-		Button elementLink = new Button(selectorCheckboxesLink);
-		elementLink.click();
-		return new CheckboxesPage();
-	}
-	
-	public ABtestPage clickABtestingLink() {
-		Button elementLink = new Button(selectorElementClick);
-		elementLink.click();
-		return new ABtestPage();
-	}
-	
-	public ChallengingDomPage clickChallengingDomLink() {
-		Button elementLink = new Button(selectorChallengingDomClick);
-		elementLink.click();
-		return new ChallengingDomPage();
-	}
-	
-	public BrokenImagePage clickBrokenImageLink() {
-		Button elementLink = new Button(selectorBrokenImageClickLink);
-		elementLink.click();
-		return new BrokenImagePage();
-	}
-	
-	public DropdownPage clickDropdownLink() {
-		Button elementLink = new Button(selectorDropdownClickLink);
-		elementLink.click();
-		return new DropdownPage();
-	}
-	
-	public KeyPressesPage clickKeyPressesLink() {
-		Button elementLink = new Button(selectorKeyPressesLink);
-		elementLink.click();
-		return new KeyPressesPage();
-	}
-	
-	public SortableDataTablesPage clickSortableDataTablesLink() {
-		Button elementLink = new Button(selectorSortableDataTablesLink);
-		elementLink.click();
-		return new SortableDataTablesPage();
-	}
-	
-	public JavaScriptAlertsPage clickJavaScriptAlertLink() {
-		Button elementLink = new Button(selectorJavaScriptAlertLink);
-		elementLink.click();
-		return new JavaScriptAlertsPage();
-	}
-	
-	public DynamicContentPage clickDynamicContentPage() {
-		Button elementLink = new Button(selectorDynamicContent);
-		elementLink.click();
-		return new DynamicContentPage();
-	}
-	
-	public MultipleWindowsPage clickmultipleWindowsPageLink() {
-		Button elementLink = new Button(selectorMultipleWindowsLink);
-		elementLink.click();
-		return new MultipleWindowsPage();
-	}
-	
-	public RedirectLinkPage clickRedirectLinkPage() {
-		Button elementLink = new Button(selectorRedirectLink);
-		elementLink.click();
-		return new RedirectLinkPage();
-	}
-	
-	public TheBasicAuthPage clickBasicAuthLink() {
-		getDriver().waitForPageLoaded();
-		WebElement link = getDriver().findElementDynamic(selectorBasicAuthLink);
-		JavascriptExecutor executor = (JavascriptExecutor) getDriver();
-		executor.executeScript("var elem=arguments[0]; setTimeout(function() {elem.click();}, 100)", link);
-		return new TheBasicAuthPage();
-	}
-	
-	public HoversPage clickHoversLink() {
-		Button elementLink = new Button(selectorHoversLink);
-		elementLink.click();
-		return new HoversPage();
-	}
-	
-	public StatusCodesHomePage clickStatusCodesLink() {
-		getDriver().findElementDynamic(selectorStatusCodesLink)
-						.click();
-		return new StatusCodesHomePage();
-	}
-	
-	public HorizontalSliderPage clickHorizontalSliderLink() {
-		WebElement elementLink = getDriver().findElementDynamic(selectorHorizontalSliderLink);
-		elementLink.click();
-		return new HorizontalSliderPage();
-	}
-	
-	public FormAuthenticationPage clickFormAuthenticationLink() {
-		WebElement elementLink = getDriver().findElementDynamic(selectorFormAuthenticationLink);
-		elementLink.click();
-		return new FormAuthenticationPage();
-	}
-	
-	public ForgotPasswordPage clickForgotPasswordLink() {
-		WebElement elementLink = getDriver().findElementDynamic(selectorForgotPasswordLink);
-		elementLink.click();
-		return new ForgotPasswordPage();
-	}
-	
-	public ExitIntentPage clickExitIntentLink() {
-		WebElement elementLink = getDriver().findElementDynamic(selectorExitIntentLink);
-		elementLink.click();
-		return new ExitIntentPage();
-	}
-	
-	public DynamicLoadingPage clickDynamicLoadingLink() {
-		WebElement elementLink = getDriver().findElementDynamic(selectorDynamicLoadingLink);
-		elementLink.click();
-		return new DynamicLoadingPage();
-	}
-	
+
+    private static final By selectorCheckboxesLink = By.cssSelector("li > a[href*=checkboxes]");
+    private static final By selectorElementClick = By.cssSelector("li > a[href*=abtest]");
+    private static final By selectorBrokenImageClickLink = By.cssSelector("li > a[href*=broken_images]");
+    private static final By selectorDropdownClickLink = By.cssSelector("li > a[href*=dropdown]");
+    private static final By selectorMultipleWindowsLink = By.cssSelector("li > a[href*=windows]");
+    private static final By selectorBasicAuthLink = By.cssSelector("li > a[href*=basic_auth]");
+    private static final By selectorKeyPressesLink = By.cssSelector("li > a[href*=key_presses]");
+    private static final By selectorRedirectLink = By.cssSelector("li > a[href*=redirector]");
+    private static final By selectorJavaScriptAlertLink = By.cssSelector("li > a[href*=javascript_alerts]");
+    private static final By selectorHoversLink = By.cssSelector("li > a[href*=hovers]");
+    private static final By selectorSortableDataTablesLink = By.cssSelector("li > a[href*=tables]");
+    private static final By selectorChallengingDomClick = By.cssSelector("li > a[href*=challenging_dom]");
+    private static final By selectorStatusCodesLink = By.cssSelector("li > a[href*=status_codes]");
+    private static final By selectorDynamicContent = By.cssSelector("li > a[href*=dynamic_content]");
+    private static final By selectorHorizontalSliderLink = By.cssSelector("li > a[href*=horizontal_slider]");
+    private static final By selectorFormAuthenticationLink = By.cssSelector("li > a[href*=login]");
+    private static final By selectorForgotPasswordLink = By.cssSelector("li > a[href*=forgot_password]");
+    private static final By selectorExitIntentLink = By.cssSelector("li > a[href*=exit_intent]");
+    private static final By selectorDynamicLoadingLink = By.cssSelector("li > a[href*=dynamic_loading]");
+
+    @Override
+    public boolean isLoaded() {
+        BFLogger.logDebug("The internet page is loaded: " + getDriver().getCurrentUrl());
+        return getDriver().getCurrentUrl()
+                .equals(GetEnvironmentParam.THE_INTERNET_MAIN_PAGE.getValue());
+    }
+
+    @Override
+    public void load() {
+        BFLogger.logDebug("load page");
+        getDriver().get(GetEnvironmentParam.THE_INTERNET_MAIN_PAGE.getValue());
+        getDriver().waitForPageLoaded();
+    }
+
+    @Override
+    public String pageTitle() {
+        return getDriver().getTitle();
+    }
+
+    public CheckboxesPage clickCheckboxesLink() {
+        Button elementLink = new Button(selectorCheckboxesLink);
+        elementLink.click();
+        return new CheckboxesPage();
+    }
+
+    public ABtestPage clickABtestingLink() {
+        Button elementLink = new Button(selectorElementClick);
+        elementLink.click();
+        return new ABtestPage();
+    }
+
+    public ChallengingDomPage clickChallengingDomLink() {
+        Button elementLink = new Button(selectorChallengingDomClick);
+        elementLink.click();
+        return new ChallengingDomPage();
+    }
+
+    public BrokenImagePage clickBrokenImageLink() {
+        Button elementLink = new Button(selectorBrokenImageClickLink);
+        elementLink.click();
+        return new BrokenImagePage();
+    }
+
+    public DropdownPage clickDropdownLink() {
+        Button elementLink = new Button(selectorDropdownClickLink);
+        elementLink.click();
+        return new DropdownPage();
+    }
+
+    public KeyPressesPage clickKeyPressesLink() {
+        Button elementLink = new Button(selectorKeyPressesLink);
+        elementLink.click();
+        return new KeyPressesPage();
+    }
+
+    public SortableDataTablesPage clickSortableDataTablesLink() {
+        Button elementLink = new Button(selectorSortableDataTablesLink);
+        elementLink.click();
+        return new SortableDataTablesPage();
+    }
+
+    public JavaScriptAlertsPage clickJavaScriptAlertLink() {
+        Button elementLink = new Button(selectorJavaScriptAlertLink);
+        elementLink.click();
+        return new JavaScriptAlertsPage();
+    }
+
+    public DynamicContentPage clickDynamicContentPage() {
+        Button elementLink = new Button(selectorDynamicContent);
+        elementLink.click();
+        return new DynamicContentPage();
+    }
+
+    public MultipleWindowsPage clickmultipleWindowsPageLink() {
+        Button elementLink = new Button(selectorMultipleWindowsLink);
+        elementLink.click();
+        return new MultipleWindowsPage();
+    }
+
+    public RedirectLinkPage clickRedirectLinkPage() {
+        Button elementLink = new Button(selectorRedirectLink);
+        elementLink.click();
+        return new RedirectLinkPage();
+    }
+
+    public TheBasicAuthPage clickBasicAuthLink() {
+        getDriver().waitForPageLoaded();
+        WebElement link = getDriver().findElementDynamic(selectorBasicAuthLink);
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        executor.executeScript("var elem=arguments[0]; setTimeout(function() {elem.click();}, 100)", link);
+        return new TheBasicAuthPage();
+    }
+
+    public HoversPage clickHoversLink() {
+        Button elementLink = new Button(selectorHoversLink);
+        elementLink.click();
+        return new HoversPage();
+    }
+
+    public StatusCodesHomePage clickStatusCodesLink() {
+        getDriver().findElementDynamic(selectorStatusCodesLink)
+                .click();
+        return new StatusCodesHomePage();
+    }
+
+    public HorizontalSliderPage clickHorizontalSliderLink() {
+        WebElement elementLink = getDriver().findElementDynamic(selectorHorizontalSliderLink);
+        elementLink.click();
+        return new HorizontalSliderPage();
+    }
+
+    public FormAuthenticationPage clickFormAuthenticationLink() {
+        WebElement elementLink = getDriver().findElementDynamic(selectorFormAuthenticationLink);
+        elementLink.click();
+        return new FormAuthenticationPage();
+    }
+
+    public ForgotPasswordPage clickForgotPasswordLink() {
+        WebElement elementLink = getDriver().findElementDynamic(selectorForgotPasswordLink);
+        elementLink.click();
+        return new ForgotPasswordPage();
+    }
+
+    public ExitIntentPage clickExitIntentLink() {
+        WebElement elementLink = getDriver().findElementDynamic(selectorExitIntentLink);
+        elementLink.click();
+        return new ExitIntentPage();
+    }
+
+    public DynamicLoadingPage clickDynamicLoadingLink() {
+        WebElement elementLink = getDriver().findElementDynamic(selectorDynamicLoadingLink);
+        elementLink.click();
+        return new DynamicLoadingPage();
+    }
+
 }
--- allure-app-under-test/src/test/java/com/capgemini/ntc/selenium/projectY/IFrameTest.java
+++ allure-app-under-test/src/test/java/com/capgemini/ntc/selenium/projectY/IFrameTest.java
@@ -1,123 +1,120 @@
 package com.capgemini.ntc.selenium.projectY;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 import com.capgemini.ntc.selenium.pages.projectY.IFramePage;
 import com.capgemini.ntc.test.core.BaseTest;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class IFrameTest extends BaseTest {
-	
-	private final static IFramePage iFramePage = new IFramePage();
-	
-	private final static String[] invocationTestData = {
-					"Litwo, Ojczyzno moja\n",
-					"Tys jest jak zdrowie"
-	};
-	
-	@Override
-	public void setUp() {
-		iFramePage.load();
-	}
-	
-	@Test
-	public void testHeaderVisibility() {
-		assertTrue(iFramePage.getHeaderVisibility());
-	}
-	
-	@Test
-	public void testEditorVisibility() {
-		assertTrue(iFramePage.getEditorVisibility());
-	}
-	
-	@Test
-	public void testMenuVisibility() {
-		assertTrue(iFramePage.getMenuVisibility());
-	}
 
-	@Test
-	public void testToolbarVisibility() {
-		assertTrue(iFramePage.getToolbarVisibility());
-	}
+    private final static IFramePage iFramePage = new IFramePage();
 
-	@Test
-	public void testTypingBoldText() {
-		iFramePage.createNewDocument();
-		iFramePage.clickBoldButton();
+    private final static String[] invocationTestData = {
+            "Litwo, Ojczyzno moja\n",
+            "Tys jest jak zdrowie"
+    };
 
-		for (String anInvocationTestData : invocationTestData) iFramePage.addTextToEditor(anInvocationTestData);
-		
-		assertEquals("p » strong", iFramePage.getStatusbarText());
-		assertEquals(invocationTestData[0].concat(invocationTestData[1]), iFramePage.getTextFromEditor());
-		iFramePage.findElements();
-		iFramePage.clearEditor();
-	}
-	
-	@Test
-	public void testTyping() {
-		iFramePage.createNewDocument();
-		typeInvocation();
-		
-		assertEquals(invocationTestData[0].concat(invocationTestData[1]), iFramePage.getTextFromEditor());
-		iFramePage.findElements();
-		iFramePage.clearEditor();
-	}
-	
-	@Test
-	public void testNewDocument() {
-		iFramePage.createNewDocument();
-		iFramePage.clickBoldButton();
-		typeInvocation();
-		iFramePage.createNewDocument();
-		assertEquals("", iFramePage.getTextFromEditor());
-		assertFalse(iFramePage.isBoldButtonPressed());
-	}
-	
-	@Test
-	public void testBoldingWithShortcut() {
-		iFramePage.createNewDocument();
-		typeInvocation();
-		iFramePage.changeWholeTextBold();
-		assertEquals("p » strong", iFramePage.getStatusbarText());
-		iFramePage.changeWholeTextBold();
-		assertEquals("p", iFramePage.getStatusbarText());
-		iFramePage.clearEditor();
-	}
-	
-	@Test
-	public void testItalicingWithShortcut() {
-		iFramePage.createNewDocument();
-		typeInvocation();
-		iFramePage.changeWholeTextItalic();
-		assertEquals("p » em", iFramePage.getStatusbarText());
-		iFramePage.changeWholeTextItalic();
-		assertEquals("p", iFramePage.getStatusbarText());
-		iFramePage.clearEditor();
-	}
-	
-	@Test
-	public void testFontAttributesChangeWithShortcut() {
-		iFramePage.createNewDocument();
-		typeInvocation();
-		iFramePage.changeWholeTextItalic();
-		iFramePage.changeWholeTextBold();
-		iFramePage.changeWholeTextBoldItalicUnderline();
-		assertEquals("p » span", iFramePage.getStatusbarText());
-		iFramePage.clearEditor();
-	}
-	
-	@Override
-	public void tearDown() {
-	}
-	
-	private void typeInvocation() {
-		iFramePage.putTextToEditor(invocationTestData[0]);
-		
-		for (int i = 1; i < invocationTestData.length; i++)
-			iFramePage.addTextToEditor(invocationTestData[i]);
-	}
-	
+    @Override
+    public void setUp() {
+        iFramePage.load();
+    }
+
+    @Test
+    public void testHeaderVisibility() {
+        assertTrue(iFramePage.getHeaderVisibility());
+    }
+
+    @Test
+    public void testEditorVisibility() {
+        assertTrue(iFramePage.getEditorVisibility());
+    }
+
+    @Test
+    public void testMenuVisibility() {
+        assertTrue(iFramePage.getMenuVisibility());
+    }
+
+    @Test
+    public void testToolbarVisibility() {
+        assertTrue(iFramePage.getToolbarVisibility());
+    }
+
+    @Test
+    public void testTypingBoldText() {
+        iFramePage.createNewDocument();
+        iFramePage.clickBoldButton();
+
+        for (String anInvocationTestData : invocationTestData) iFramePage.addTextToEditor(anInvocationTestData);
+
+        assertEquals("p » strong", iFramePage.getStatusbarText());
+        assertEquals(invocationTestData[0].concat(invocationTestData[1]), iFramePage.getTextFromEditor());
+        iFramePage.findElements();
+        iFramePage.clearEditor();
+    }
+
+    @Test
+    public void testTyping() {
+        iFramePage.createNewDocument();
+        typeInvocation();
+
+        assertEquals(invocationTestData[0].concat(invocationTestData[1]), iFramePage.getTextFromEditor());
+        iFramePage.findElements();
+        iFramePage.clearEditor();
+    }
+
+    @Test
+    public void testNewDocument() {
+        iFramePage.createNewDocument();
+        iFramePage.clickBoldButton();
+        typeInvocation();
+        iFramePage.createNewDocument();
+        assertEquals("", iFramePage.getTextFromEditor());
+        assertFalse(iFramePage.isBoldButtonPressed());
+    }
+
+    @Test
+    public void testBoldingWithShortcut() {
+        iFramePage.createNewDocument();
+        typeInvocation();
+        iFramePage.changeWholeTextBold();
+        assertEquals("p » strong", iFramePage.getStatusbarText());
+        iFramePage.changeWholeTextBold();
+        assertEquals("p", iFramePage.getStatusbarText());
+        iFramePage.clearEditor();
+    }
+
+    @Test
+    public void testItalicingWithShortcut() {
+        iFramePage.createNewDocument();
+        typeInvocation();
+        iFramePage.changeWholeTextItalic();
+        assertEquals("p » em", iFramePage.getStatusbarText());
+        iFramePage.changeWholeTextItalic();
+        assertEquals("p", iFramePage.getStatusbarText());
+        iFramePage.clearEditor();
+    }
+
+    @Test
+    public void testFontAttributesChangeWithShortcut() {
+        iFramePage.createNewDocument();
+        typeInvocation();
+        iFramePage.changeWholeTextItalic();
+        iFramePage.changeWholeTextBold();
+        iFramePage.changeWholeTextBoldItalicUnderline();
+        assertEquals("p » span", iFramePage.getStatusbarText());
+        iFramePage.clearEditor();
+    }
+
+    @Override
+    public void tearDown() {
+    }
+
+    private void typeInvocation() {
+        iFramePage.putTextToEditor(invocationTestData[0]);
+
+        for (int i = 1; i < invocationTestData.length; i++)
+            iFramePage.addTextToEditor(invocationTestData[i]);
+    }
+
 }
