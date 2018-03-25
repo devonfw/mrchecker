1540762ba4fcbf7f8713de88c7d0b0b07ecd8a91
krzysztofdrzazga <krzysztof.drzazga@capgemini.com>
2018-03-25 10:04:43 +0200

    Introduced the moand Optional to deal with null comparision. Corrected indentation

--- allure-app-under-test/src/main/java/com/capgemini/ntc/selenium/pages/projectY/LargeDeepDomPage.java
+++ allure-app-under-test/src/main/java/com/capgemini/ntc/selenium/pages/projectY/LargeDeepDomPage.java
@@ -1,168 +1,164 @@
 package com.capgemini.ntc.selenium.pages.projectY;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-
 import com.capgemini.ntc.selenium.core.BasePage;
 import com.capgemini.ntc.selenium.pages.environment.GetEnvironmentParam;
 import com.capgemini.ntc.selenium.pages.environment.PageSubURLsProjectYEnum;
 import com.capgemini.ntc.test.core.logger.BFLogger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class LargeDeepDomPage extends BasePage {
-	
-	private static final int	EXP_TBL_HDR_CELLS_CNT		= 50;
-	private final static String	URL							= GetEnvironmentParam.THE_INTERNET_MAIN_PAGE.getValue()
-			+ PageSubURLsProjectYEnum.LARGE_DEEP_DOM.getValue();
-	private static final By		mainHeaderLocator			= By.xpath("//div[@class='example']/h3");
-	private static final By		noSiblingsHeaderLocator		= By.xpath("//div[@class='example']/h4[1]");
-	private static final By		siblingsHeaderLocator		= By.xpath("//div[@class='example']/h4[2]");
-	private static final By		descriptionParagraphLocator	= By.xpath("//div[@class='example']/p[1]");
-	private static final By		noSiblingsParagraphLocator	= By.id("no-siblings");
-	private static final By		siblingsDivLocator			= By.id("no-siblings");
-	private static final By		largeTableLocator			= By.id("large-table");
-	
-	private static WebElement				mainHeader;
-	private static WebElement				noSiblingsHeader;
-	private static WebElement				siblingsHeader;
-	private static WebElement				descriptionParagraph;
-	private static WebElement				noSiblingsParagraph;
-	private static WebElement				siblingsDiv;
-	private static WebElement				largeTable;
-	private static final List<WebElement>	siblings	= new ArrayList<>();
-	private static final List<WebElement>	headerCells	= new ArrayList<>(EXP_TBL_HDR_CELLS_CNT);
-	private static final List<WebElement>	tableRows	= new ArrayList<>(EXP_TBL_HDR_CELLS_CNT);
-	
-	@Override
-	public void load() {
-		getDriver().get(URL);
-		BFLogger.logInfo("Page " + URL + " is loaded = " + isLoaded());
-		
-		findElements();
-	}
-	
-	private void findElements() {
-		mainHeader = getDriver().findElementQuietly(mainHeaderLocator);
-		noSiblingsHeader = getDriver().findElementQuietly(noSiblingsHeaderLocator);
-		siblingsHeader = getDriver().findElementQuietly(siblingsHeaderLocator);
-		descriptionParagraph = getDriver().findElementQuietly(descriptionParagraphLocator);
-		noSiblingsParagraph = getDriver().findElementQuietly(noSiblingsParagraphLocator);
-		siblingsDiv = getDriver().findElementQuietly(siblingsDivLocator);
-		largeTable = getDriver().findElementQuietly(largeTableLocator);
-		
-		int hdrCellIndex = 1;
-		headerCells.clear();
-		while (hdrCellIndex <= EXP_TBL_HDR_CELLS_CNT) {
-			By byHdrCellLocator = By.id("header-" + String.valueOf(hdrCellIndex));
-			WebElement hdrCell = getDriver().findElementQuietly(byHdrCellLocator);
-			if (hdrCell != null)
-				headerCells.add(hdrCell);
-			hdrCellIndex++;
-		}
-		
-		int tableRowIndex = 1;
-		tableRows.clear();
-		while (tableRowIndex <= EXP_TBL_HDR_CELLS_CNT) {
-			By byRowLocator = By.className("row-" + String.valueOf(tableRowIndex));
-			WebElement row = getDriver().findElementQuietly(byRowLocator);
-			if (row != null)
-				tableRows.add(row);
-			tableRowIndex++;
-		}
-	}
-	
-	public List<String> getTableHeaderContent() {
-		findElements();
-		List<String> headers = new ArrayList<>();
-		for (WebElement hdrCell : headerCells) {
-			headers.add(hdrCell.getText());
-		}
-		return headers;
-	}
-	
-	public String getCell(int rowIndex, int columnIndex) {
-		findElements();
-		
-		if (rowIndex < 1 || rowIndex > tableRows.size()) {
-			BFLogger.logError("No row with index " + rowIndex + " in Table. Available rows: 1-" + tableRows.size());
-			return "";
-		} else {
-			
-			WebElement cell = getDriver().findElementQuietly(tableRows.get(rowIndex - 1), By.className("column-" + String.valueOf(columnIndex)));
-			return (cell != null) ? cell.getText() : "";
-		}
-	}
-	
-	public boolean areElementsVisible() {
-		return mainHeader.isDisplayed()
-				&& noSiblingsHeader.isDisplayed()
-				&& siblingsHeader.isDisplayed()
-				&& descriptionParagraph.isDisplayed()
-				&& noSiblingsParagraph.isDisplayed()
-				&& siblingsDiv.isDisplayed()
-				&& largeTable.isDisplayed();
-	}
-	
-	public int getSiblingTiersCount() {
-		By numberedSiblingsLocator = By.xpath("//div[contains(@class, 'parent') and contains(@class, 'large-12')]");
-		// List<WebElement> numberedSiblings = getDriver().findElementDynamics(numberedSiblingsLocator);
-		return Optional.ofNullable(getDriver().findElementDynamics(numberedSiblingsLocator))
-				.map(List::size)
-				.orElse(0);
-		/*
-		 * return (numberedSiblings == null)
-		 * ? 0
-		 * : numberedSiblings.size();
-		 */}
-		
-	public String getSiblingDivText(int tier, int item) {
-		return (item == 1)
-				? getParentSiblingDivHeader(tier)
-				: getNonParentSiblingDivText(tier, item);
-	}
-	
-	public String getParentSiblingDivHeader(int tier) {
-		String siblingText = getWholeParentSiblingDiv(tier);
-		
-		return ("".equals(siblingText))
-				? ""
-				: siblingText
-						.substring(0, siblingText.indexOf('\n'));
-	}
-	
-	public String getWholeParentSiblingDiv(int tier) {
-		By siblingSelector = By.cssSelector(".parent.large-12.tier-" + String.valueOf(tier) + ".item-1");
-		
-		WebElement sibling = getDriver().findElementQuietly(siblingSelector);
-		if (sibling == null)
-			return "";
-		else {
-			return sibling.getText();
-		}
-	}
-	
-	private String getNonParentSiblingDivText(int tier, int item) {
-		By siblingSelector = By.cssSelector(".large-12.tier-" + String.valueOf(tier) + ".item-" + String.valueOf(item));
-		
-		WebElement sibling = getDriver().findElementQuietly(siblingSelector);
-		return (sibling == null) ? "" : sibling.getText();
-	}
-	
-	@Override
-	public String pageTitle() {
-		return getDriver().getTitle();
-	}
-	
-	@Override
-	public boolean isLoaded() {
-		boolean loadCompleted = ((JavascriptExecutor) getDriver()).executeScript("return document.readyState")
-				.equals("complete");
-		
-		return loadCompleted && isUrlAndPageTitleAsCurrentPage(URL);
-	}
-	
+
+    private static final int EXP_TBL_HDR_CELLS_CNT = 50;
+    private final static String URL = GetEnvironmentParam.THE_INTERNET_MAIN_PAGE.getValue()
+            + PageSubURLsProjectYEnum.LARGE_DEEP_DOM.getValue();
+    private static final By mainHeaderLocator = By.xpath("//div[@class='example']/h3");
+    private static final By noSiblingsHeaderLocator = By.xpath("//div[@class='example']/h4[1]");
+    private static final By siblingsHeaderLocator = By.xpath("//div[@class='example']/h4[2]");
+    private static final By descriptionParagraphLocator = By.xpath("//div[@class='example']/p[1]");
+    private static final By noSiblingsParagraphLocator = By.id("no-siblings");
+    private static final By siblingsDivLocator = By.id("no-siblings");
+    private static final By largeTableLocator = By.id("large-table");
+
+    private static WebElement mainHeader;
+    private static WebElement noSiblingsHeader;
+    private static WebElement siblingsHeader;
+    private static WebElement descriptionParagraph;
+    private static WebElement noSiblingsParagraph;
+    private static WebElement siblingsDiv;
+    private static WebElement largeTable;
+    private static final List<WebElement> siblings = new ArrayList<>();
+    private static final List<WebElement> headerCells = new ArrayList<>(EXP_TBL_HDR_CELLS_CNT);
+    private static final List<WebElement> tableRows = new ArrayList<>(EXP_TBL_HDR_CELLS_CNT);
+
+    @Override
+    public void load() {
+        getDriver().get(URL);
+        BFLogger.logInfo("Page " + URL + " is loaded = " + isLoaded());
+
+        findElements();
+    }
+
+    private void findElements() {
+        mainHeader = getDriver().findElementQuietly(mainHeaderLocator);
+        noSiblingsHeader = getDriver().findElementQuietly(noSiblingsHeaderLocator);
+        siblingsHeader = getDriver().findElementQuietly(siblingsHeaderLocator);
+        descriptionParagraph = getDriver().findElementQuietly(descriptionParagraphLocator);
+        noSiblingsParagraph = getDriver().findElementQuietly(noSiblingsParagraphLocator);
+        siblingsDiv = getDriver().findElementQuietly(siblingsDivLocator);
+        largeTable = getDriver().findElementQuietly(largeTableLocator);
+
+        int hdrCellIndex = 1;
+        headerCells.clear();
+        while (hdrCellIndex <= EXP_TBL_HDR_CELLS_CNT) {
+            By byHdrCellLocator = By.id("header-" + String.valueOf(hdrCellIndex));
+            WebElement hdrCell = getDriver().findElementQuietly(byHdrCellLocator);
+            if (hdrCell != null)
+                headerCells.add(hdrCell);
+            hdrCellIndex++;
+        }
+
+        int tableRowIndex = 1;
+        tableRows.clear();
+        while (tableRowIndex <= EXP_TBL_HDR_CELLS_CNT) {
+            By byRowLocator = By.className("row-" + String.valueOf(tableRowIndex));
+            WebElement row = getDriver().findElementQuietly(byRowLocator);
+            if (row != null)
+                tableRows.add(row);
+            tableRowIndex++;
+        }
+    }
+
+    public List<String> getTableHeaderContent() {
+        findElements();
+        List<String> headers = new ArrayList<>();
+        for (WebElement hdrCell : headerCells) {
+            headers.add(hdrCell.getText());
+        }
+        return headers;
+    }
+
+    public String getCell(int rowIndex, int columnIndex) {
+        findElements();
+
+        if (rowIndex < 1 || rowIndex > tableRows.size()) {
+            BFLogger.logError("No row with index " + rowIndex + " in Table. Available rows: 1-" + tableRows.size());
+            return "";
+        } else {
+
+            WebElement cell = getDriver().findElementQuietly(tableRows.get(rowIndex - 1), By.className("column-" + String.valueOf(columnIndex)));
+            return Optional.ofNullable(cell)
+                    .map(WebElement::getText)
+                    .orElse("");
+        }
+    }
+
+    public boolean areElementsVisible() {
+        return mainHeader.isDisplayed()
+                && noSiblingsHeader.isDisplayed()
+                && siblingsHeader.isDisplayed()
+                && descriptionParagraph.isDisplayed()
+                && noSiblingsParagraph.isDisplayed()
+                && siblingsDiv.isDisplayed()
+                && largeTable.isDisplayed();
+    }
+
+    public int getSiblingTiersCount() {
+        By numberedSiblingsLocator = By.xpath("//div[contains(@class, 'parent') and contains(@class, 'large-12')]");
+        return Optional.ofNullable(getDriver().findElementDynamics(numberedSiblingsLocator))
+                .map(List::size)
+                .orElse(0);
+    }
+
+    public String getSiblingDivText(int tier, int item) {
+        return (item == 1)
+                ? getParentSiblingDivHeader(tier)
+                : getNonParentSiblingDivText(tier, item);
+    }
+
+    public String getParentSiblingDivHeader(int tier) {
+        String siblingText = getWholeParentSiblingDiv(tier);
+
+        return ("".equals(siblingText))
+                ? ""
+                : siblingText
+                .substring(0, siblingText.indexOf('\n'));
+    }
+
+    public String getWholeParentSiblingDiv(int tier) {
+        By siblingSelector = By.cssSelector(".parent.large-12.tier-" + String.valueOf(tier) + ".item-1");
+
+        WebElement sibling = getDriver().findElementQuietly(siblingSelector);
+        return Optional.ofNullable(sibling)
+                .map(WebElement::getText)
+                .orElse("");
+    }
+
+    private String getNonParentSiblingDivText(int tier, int item) {
+        By siblingSelector = By.cssSelector(".large-12.tier-" + String.valueOf(tier) + ".item-" + String.valueOf(item));
+
+        WebElement sibling = getDriver().findElementQuietly(siblingSelector);
+        return Optional.ofNullable(sibling)
+                .map(WebElement::getText)
+                .orElse("");
+    }
+
+    @Override
+    public String pageTitle() {
+        return getDriver().getTitle();
+    }
+
+    @Override
+    public boolean isLoaded() {
+        boolean loadCompleted = ((JavascriptExecutor) getDriver()).executeScript("return document.readyState")
+                .equals("complete");
+
+        return loadCompleted && isUrlAndPageTitleAsCurrentPage(URL);
+    }
+
 }
--- allure-app-under-test/src/main/java/com/capgemini/ntc/selenium/pages/projectY/NestedFramesPage.java
+++ allure-app-under-test/src/main/java/com/capgemini/ntc/selenium/pages/projectY/NestedFramesPage.java
@@ -1,137 +1,143 @@
 package com.capgemini.ntc.selenium.pages.projectY;
 
-import com.capgemini.ntc.selenium.core.BasePage;
-import com.capgemini.ntc.selenium.pages.environment.GetEnvironmentParam;
-import com.capgemini.ntc.selenium.pages.environment.PageSubURLsProjectYEnum;
-import com.capgemini.ntc.test.core.logger.BFLogger;
+import java.util.Optional;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
+import com.capgemini.ntc.selenium.core.BasePage;
+import com.capgemini.ntc.selenium.pages.environment.GetEnvironmentParam;
+import com.capgemini.ntc.selenium.pages.environment.PageSubURLsProjectYEnum;
+import com.capgemini.ntc.test.core.logger.BFLogger;
+
 public class NestedFramesPage extends BasePage {
-
-    public enum Frame {
-        TOP,
-        LEFT,
-        MIDDLE,
-        RIGHT,
-        BOTTOM;
-
-        public String getName() {
-            switch (this) {
-                case TOP:
-                    return "frame-top";
-                case LEFT:
-                    return "frame-left";
-                case MIDDLE:
-                    return "frame-middle";
-                case RIGHT:
-                    return "frame-right";
-                case BOTTOM:
-                    return "frame-bottom";
-                default:
-                    return "";
-            }
-        }
-    }
-
-    private static final String URL = GetEnvironmentParam.THE_INTERNET_MAIN_PAGE.getValue()
-            + PageSubURLsProjectYEnum.NESTED_FRAMES.getValue();
-
-    private static WebElement topFrame;
-    private static WebElement leftFrame;
-    private static WebElement middleFrame;
-    private static WebElement rightFrame;
-    private static WebElement bottomFrame;
-
-    private static final By topFrameLocalizer = By.name(Frame.TOP.getName());
-    private static final By leftFrameLocalizer = By.name(Frame.LEFT.getName());
-    private static final By middleFrameLocalizer = By.name(Frame.MIDDLE.getName());
-    private static final By rightFrameLocalizer = By.name(Frame.RIGHT.getName());
-    private static final By bottomFrameLocalizer = By.name(Frame.BOTTOM.getName());
-
-    @Override
-    public void load() {
-        BFLogger.logInfo("Page " + URL + " is loaded = " + isLoaded());
-        getDriver().get(URL);
-        findElements();
-        BFLogger.logInfo("Page " + URL + " is loaded = " + isLoaded());
-    }
-
-    public boolean areFramesDisplayed() {
-        boolean leftFrameDisplayed = false;
-        boolean middleFrameDisplayed = false;
-        boolean rightFrameDisplayed = false;
-
-        if (topFrame != null) {
-            getDriver().switchTo()
-                    .frame(Frame.TOP.getName());
-
-            leftFrameDisplayed = leftFrame.isDisplayed();
-            middleFrameDisplayed = middleFrame.isDisplayed();
-            rightFrameDisplayed = rightFrame.isDisplayed();
-
-            getDriver().switchTo()
-                    .defaultContent();
-        }
-
-        return leftFrameDisplayed
-                && middleFrameDisplayed
-                && rightFrameDisplayed
-                && bottomFrame.isDisplayed();
-    }
-
-    private void findElements() {
-        topFrame = getDriver().findElementQuietly(topFrameLocalizer);
-        bottomFrame = getDriver().findElementQuietly(bottomFrameLocalizer);
-
-        if (topFrame != null) {
-            getDriver().switchTo()
-                    .frame(Frame.TOP.getName());
-
-            leftFrame = getDriver().findElementQuietly(leftFrameLocalizer);
-            middleFrame = getDriver().findElementQuietly(middleFrameLocalizer);
-            rightFrame = getDriver().findElementQuietly(rightFrameLocalizer);
-
-            getDriver().switchTo()
-                    .defaultContent();
-        }
-    }
-
-    public String getFrameContent(Frame frame) {
-
-        if (frame.equals(Frame.TOP) || frame.equals(Frame.BOTTOM))
-            return getOuterFrameContent(frame);
-        else
-            return getInnerFrameContent(frame);
-    }
-
-    private String getOuterFrameContent(Frame frame) {
-        getDriver().switchTo()
-                .frame(frame.getName());
-        WebElement body = getDriver().findElementQuietly(By.tagName("body"));
-        String content = (body == null) ? "" : body.getText();
-        getDriver().switchTo()
-                .defaultContent();
-        return content;
-    }
-
-    private String getInnerFrameContent(Frame frame) {
-        getDriver().switchTo()
-                .frame(Frame.TOP.getName());
-        return getOuterFrameContent(frame);
-    }
-
-    @Override
-    private boolean isLoaded() {
-        boolean loadCompleted = ((JavascriptExecutor) getDriver()).executeScript("return document.readyState")
-                .equals("complete");
-
-        return loadCompleted && isUrlAndPageTitleAsCurrentPage(URL);
-    }
-
-    @Override
-    public String pageTitle() {
-        return getDriver().getTitle();
-    }
+	
+	public enum Frame {
+		TOP,
+		LEFT,
+		MIDDLE,
+		RIGHT,
+		BOTTOM;
+		
+		public String getName() {
+			switch (this) {
+				case TOP:
+					return "frame-top";
+				case LEFT:
+					return "frame-left";
+				case MIDDLE:
+					return "frame-middle";
+				case RIGHT:
+					return "frame-right";
+				case BOTTOM:
+					return "frame-bottom";
+				default:
+					return "";
+			}
+		}
+	}
+	
+	private static final String URL = GetEnvironmentParam.THE_INTERNET_MAIN_PAGE.getValue()
+			+ PageSubURLsProjectYEnum.NESTED_FRAMES.getValue();
+	
+	private static WebElement	topFrame;
+	private static WebElement	leftFrame;
+	private static WebElement	middleFrame;
+	private static WebElement	rightFrame;
+	private static WebElement	bottomFrame;
+	
+	private static final By	topFrameLocalizer		= By.name(Frame.TOP.getName());
+	private static final By	leftFrameLocalizer		= By.name(Frame.LEFT.getName());
+	private static final By	middleFrameLocalizer	= By.name(Frame.MIDDLE.getName());
+	private static final By	rightFrameLocalizer		= By.name(Frame.RIGHT.getName());
+	private static final By	bottomFrameLocalizer	= By.name(Frame.BOTTOM.getName());
+	
+	@Override
+	public void load() {
+		BFLogger.logInfo("Page " + URL + " is loaded = " + isLoaded());
+		getDriver().get(URL);
+		findElements();
+		BFLogger.logInfo("Page " + URL + " is loaded = " + isLoaded());
+	}
+	
+	public boolean areFramesDisplayed() {
+		boolean leftFrameDisplayed = false;
+		boolean middleFrameDisplayed = false;
+		boolean rightFrameDisplayed = false;
+		
+		if (topFrame != null) {
+			getDriver().switchTo()
+					.frame(Frame.TOP.getName());
+			
+			leftFrameDisplayed = leftFrame.isDisplayed();
+			middleFrameDisplayed = middleFrame.isDisplayed();
+			rightFrameDisplayed = rightFrame.isDisplayed();
+			
+			getDriver().switchTo()
+					.defaultContent();
+		}
+		
+		return leftFrameDisplayed
+				&& middleFrameDisplayed
+				&& rightFrameDisplayed
+				&& bottomFrame.isDisplayed();
+	}
+	
+	private void findElements() {
+		topFrame = getDriver().findElementQuietly(topFrameLocalizer);
+		bottomFrame = getDriver().findElementQuietly(bottomFrameLocalizer);
+		
+		if (topFrame != null) {
+			getDriver().switchTo()
+					.frame(Frame.TOP.getName());
+			
+			leftFrame = getDriver().findElementQuietly(leftFrameLocalizer);
+			middleFrame = getDriver().findElementQuietly(middleFrameLocalizer);
+			rightFrame = getDriver().findElementQuietly(rightFrameLocalizer);
+			
+			getDriver().switchTo()
+					.defaultContent();
+		}
+	}
+	
+	public String getFrameContent(Frame frame) {
+		
+		if (frame.equals(Frame.TOP) || frame.equals(Frame.BOTTOM))
+			return getOuterFrameContent(frame);
+		else
+			return getInnerFrameContent(frame);
+	}
+	
+	private String getOuterFrameContent(Frame frame) {
+		getDriver().switchTo()
+				.frame(frame.getName());
+		WebElement body = getDriver().findElementQuietly(By.tagName("body"));
+		String content = Optional.ofNullable(body)
+				.map(WebElement::getText)
+				.orElse("");
+		
+		getDriver().switchTo()
+				.defaultContent();
+		return content;
+	}
+	
+	private String getInnerFrameContent(Frame frame) {
+		getDriver().switchTo()
+				.frame(Frame.TOP.getName());
+		return getOuterFrameContent(frame);
+	}
+	
+	@Override
+	public boolean isLoaded() {
+		boolean loadCompleted = ((JavascriptExecutor) getDriver()).executeScript("return document.readyState")
+				.equals("complete");
+		
+		return loadCompleted && isUrlAndPageTitleAsCurrentPage(URL);
+	}
+	
+	@Override
+	public String pageTitle() {
+		return getDriver().getTitle();
+	}
 }
