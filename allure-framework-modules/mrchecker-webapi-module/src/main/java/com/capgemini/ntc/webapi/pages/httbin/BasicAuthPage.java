05a69d6694a3855d0adf950c5f2f4953dca854bc
pawel-szczesny <pawel.szczesny@capgemini.com>
2018-05-01 18:52:19 +0200

    HTTPBin BasicAuth test done

---
+++ allure-framework-modules/mrchecker-webapi-module/src/main/java/com/capgemini/ntc/webapi/pages/httbin/BasicAuthPage.java
@@ -0,0 +1,27 @@
+package com.capgemini.ntc.webapi.pages.httbin;
+
+import com.capgemini.ntc.webapi.core.BasePageWebAPI;
+import com.capgemini.ntc.webapi.core.base.driver.DriverManager;
+import com.capgemini.ntc.webapi.pages.environment.GetEnvironmentParam;
+
+import io.restassured.response.Response;
+
+public class BasicAuthPage extends BasePageWebAPI {
+	
+	private final static String	HOSTNAME	= GetEnvironmentParam.HTTPBIN.getValue();
+	private final static String	PATH		= "/basic-auth/user/passwd";
+	private final static String	ENDPOINT	= HOSTNAME + PATH;
+	
+	public Response sendBasicAuthGETQuery(String username, String password) {
+		return DriverManager.getDriverWebAPI()
+						.auth()
+						.basic(username, password)
+						.when()
+						.get(ENDPOINT);
+	}
+	
+	@Override
+	public String getEndpoint() {
+		return ENDPOINT;
+	}
+}
