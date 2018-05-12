50c5eab30d4d2a2bd1bad53dfb5f3fe4568f3024
pawel-szczesny <pawel.szczesny@capgemini.com>
2018-05-12 22:41:36 +0200

    expanded page class by setters

--- allure-framework-modules/mrchecker-webapi-module/src/main/java/com/capgemini/ntc/webapi/pages/httbin/SimplePOSTPage.java
+++ allure-framework-modules/mrchecker-webapi-module/src/main/java/com/capgemini/ntc/webapi/pages/httbin/SimplePOSTPage.java
@@ -1,7 +1,11 @@
 package com.capgemini.ntc.webapi.pages.httbin;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.capgemini.ntc.webapi.core.BasePageWebAPI;
 import com.capgemini.ntc.webapi.core.base.driver.DriverManager;
@@ -12,27 +16,28 @@ import io.restassured.response.Response;
 
 public class SimplePOSTPage extends BasePageWebAPI {
 	
-	private final static String					HOSTNAME	= GetEnvironmentParam.HTTPBIN.getValue();
-	private final static String					PATH		= "/post";
-	private final static String					ENDPOINT	= HOSTNAME + PATH;
-	private final static Map<String, Object>	jsonAsMap	= new HashMap<>();
+	private final static String	HOSTNAME	= GetEnvironmentParam.HTTPBIN.getValue();
+	private final static String	PATH		= "/post";
+	private final static String	ENDPOINT	= HOSTNAME + PATH;
+	
+	private static Map<String, Object> requestFields = new HashMap<>();
 	
 	static {
-		jsonAsMap.put("comments", "Delivery Instruction");
-		jsonAsMap.put("custemail", "email");
-		jsonAsMap.put("custname", "Name");
-		jsonAsMap.put("custtel", "Telephone");
-		jsonAsMap.put("delivery", "Telephone");
-		jsonAsMap.put("size", "large");
-		jsonAsMap.put("topping", "bacon");
-		jsonAsMap.put("toppiasdasdng", "baconasdads");
-		
+		requestFields.put("custname", "");
+		requestFields.put("custtel", "");
+		requestFields.put("custemail", "");
+		requestFields.put("comments", "");
+		requestFields.put("delivery", "");
+		requestFields.put("size", "");
+		requestFields.put("topping", "");
 	}
 	
 	public Response sendPOSTQuery() {
+		// BFLogger.logInfo(Arrays.toString(requestFields.entrySet()
+		// .toArray()));
 		return DriverManager.getDriverWebAPI()
 						.contentType(ContentType.URLENC)
-						.formParams(jsonAsMap)
+						.formParams(requestFields)
 						.when()
 						.post(ENDPOINT);
 	}
@@ -41,4 +46,71 @@ public class SimplePOSTPage extends BasePageWebAPI {
 	public String getEndpoint() {
 		return ENDPOINT;
 	}
+	
+	public void setCustomerName(String customerName) {
+		requestFields.replace("custname", customerName);
+	}
+	
+	public void setTelephoneNumber(String telephoneNumber) {
+		requestFields.replace("custtel", telephoneNumber);
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		requestFields.replace("custemail", emailAddress);
+	}
+	
+	public void pickPizzaSize(PizzaSize pizzaSize) {
+		requestFields.replace("size", pizzaSize.getPizzaSizeValue());
+	}
+	
+	public void pickPizzaToppings(PizzaToppings... pizzaToppings) {
+		List<PizzaToppings> toppings = new ArrayList<>(Arrays.asList(pizzaToppings));
+		List<String> nb = toppings.stream()
+						.map(element -> element.getPizzaToppingValue())
+						.collect(Collectors.toList());
+		requestFields.replace("topping", nb);
+		
+	}
+	
+	public void setPrefferedDeliveryTime(int hours, int minutes) {
+		requestFields.replace("delivery", hours + ":" + minutes);
+	}
+	
+	public void setDeliveryInstruction(String deliveryInstruction) {
+		requestFields.put("comments", deliveryInstruction);
+	}
+	
+	public enum PizzaSize {
+		SMALL("small"),
+		MEDIUM("medium"),
+		LARGE("large");
+		
+		private String pizzaSizeValue;
+		
+		PizzaSize(String pizzaSizeValue) {
+			this.pizzaSizeValue = pizzaSizeValue;
+		}
+		
+		public String getPizzaSizeValue() {
+			return pizzaSizeValue;
+		}
+	}
+	
+	public enum PizzaToppings {
+		BACON("bacon"),
+		EXTRA_CHEESE("cheese"),
+		ONION("onion"),
+		MUSHROOM("mushroom");
+		
+		private String pizzaToppingValue;
+		
+		PizzaToppings(String pizzaToppingValue) {
+			this.pizzaToppingValue = pizzaToppingValue;
+		}
+		
+		public String getPizzaToppingValue() {
+			return pizzaToppingValue;
+		}
+	}
+	
 }
--- allure-framework-modules/mrchecker-webapi-module/src/test/java/com/capgemini/ntc/webapi/httpbin/SimplePOSTTest.java
+++ allure-framework-modules/mrchecker-webapi-module/src/test/java/com/capgemini/ntc/webapi/httpbin/SimplePOSTTest.java
@@ -8,17 +8,17 @@ import org.junit.Test;
 import com.capgemini.ntc.test.core.logger.BFLogger;
 import com.capgemini.ntc.webapi.BasePageWebApiTest;
 import com.capgemini.ntc.webapi.pages.httbin.SimplePOSTPage;
+import com.capgemini.ntc.webapi.pages.httbin.SimplePOSTPage.PizzaSize;
+import com.capgemini.ntc.webapi.pages.httbin.SimplePOSTPage.PizzaToppings;
 
 import io.restassured.response.Response;
 
 public class SimplePOSTTest extends BasePageWebApiTest {
 	
-	private SimplePOSTPage simplePOSTPage;
+	private SimplePOSTPage simplePOSTPage = new SimplePOSTPage();
 	
 	@Test
 	public void sendSimplePOSTQuery() {
-		simplePOSTPage = new SimplePOSTPage();
-		
 		BFLogger.logInfo("Step 1 - Sending POST query to " + simplePOSTPage.getEndpoint());
 		Response response = simplePOSTPage.sendPOSTQuery();
 		
@@ -30,4 +30,23 @@ public class SimplePOSTTest extends BasePageWebApiTest {
 						.asString());
 	}
 	
+	@Test
+	public void fillFormWithValidData() {
+		simplePOSTPage.setCustomerName("Jan Kowalski");
+		simplePOSTPage.setTelephoneNumber("123456789");
+		simplePOSTPage.setEmailAddress("jan.kowalski@email.com");
+		simplePOSTPage.pickPizzaSize(PizzaSize.LARGE);
+		simplePOSTPage.pickPizzaToppings(PizzaToppings.BACON, PizzaToppings.EXTRA_CHEESE);
+		simplePOSTPage.setPrefferedDeliveryTime(12, 30);
+		simplePOSTPage.setDeliveryInstruction("8 floor");
+		
+		Response response = simplePOSTPage.sendPOSTQuery();
+		
+		BFLogger.logInfo("Step 2 - Validate response status code: ");
+		assertThat(response.statusCode(), is(200));
+		
+		BFLogger.logInfo("RESPONSE Body " + response.thenReturn()
+						.body()
+						.asString());
+	}
 }
