e4995e4dd8f38fc6960785fa44bee99d29fb0d45
Przemysław Wojtków <35295484+przwojtkow@users.noreply.github.com>
2018-05-14 13:40:40 +0200

    AbstractDao class added. (#106)

--- allure-framework-modules/mrchecker-database-module/src/main/java/com/capgemini/ntc/database/core/DriverManager.java
+++ allure-framework-modules/mrchecker-database-module/src/main/java/com/capgemini/ntc/database/core/DriverManager.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class DBDriverManager {
+public class DriverManager {
 
 	private static EntityManagerFactory emf;
 
