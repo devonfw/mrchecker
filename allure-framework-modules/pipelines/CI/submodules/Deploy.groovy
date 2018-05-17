dd74af5811fc2085830450e553c353d19def5e6c
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2018-05-17 20:30:38 +0200

    upgrade Deploy process

--- allure-framework-modules/pipelines/CI/submodules/Deploy.groovy
+++ allure-framework-modules/pipelines/CI/submodules/Deploy.groovy
@@ -1,13 +1,20 @@
 def call(){
 
 //# Build deployable app
-    stage('Deploy'){
+    deployToLocalRepo();
+}
+
+
+
+def private void deployToLocalRepo(){
+    stage('Install to local repo'){
         sh"""
             export BASE_PATH=.
             cd ${env.APP_WORKSPACE}
-            mvn -q deploy -DskipTests=true
+            mvn -q install -DskipTests=true
         """
         archiveArtifacts"${env.APP_WORKSPACE}target/*.jar"
     }
 }
+
 return this
