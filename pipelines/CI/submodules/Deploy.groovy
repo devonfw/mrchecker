41f4e2891ecfed4af186ea4430ddfb09baa9111b
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2018-05-16 23:55:15 -0400

    update Deploy groovy after Local Repo changed

--- pipelines/CI/submodules/Deploy.groovy
+++ pipelines/CI/submodules/Deploy.groovy
@@ -3,8 +3,7 @@ def call(){
 //# Build deployable app
     stage('Deploy'){
         sh"""
-            mkdir -p ${env.WORKSPACE}/m2/repository
-            set BASE_PATH=${env.WORKSPACE}
+            export BASE_PATH=.
             cd ${env.APP_WORKSPACE}
             mvn -q deploy -DskipTests=true
         """
