fa971b43df121623d74375ddc14b3c22e906c7c1
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2018-05-20 00:14:17 +0200

    Fix.  Version is empty or null

--- allure-framework-modules/pipelines/CI/submodules/Deploy.groovy
+++ allure-framework-modules/pipelines/CI/submodules/Deploy.groovy
@@ -9,17 +9,17 @@ def call(){
 def private void deployToLocalRepo(String version){
     stage('Install to local repo'){
         echo("version: -${version}-");
-        if (version != null || version.isEmpty()){
+        if (version == null || version.isEmpty()){
             sh"""
                 export BASE_PATH=.
                 cd ${env.APP_WORKSPACE}
-                mvn install -Dversion=${version} -DskipTests=true ${env.MVN_PARAMETERS}
+                mvn install -DskipTests=true ${env.MVN_PARAMETERS}
             """
         } else {
             sh"""
                 export BASE_PATH=.
                 cd ${env.APP_WORKSPACE}
-                mvn install -DskipTests=true ${env.MVN_PARAMETERS}
+                mvn install -Dversion=${version} -DskipTests=true ${env.MVN_PARAMETERS}
             """
         }
         archiveArtifacts allowEmptyArchive: true, artifacts: "${env.APP_WORKSPACE}target/*.jar";
@@ -33,15 +33,15 @@ def private void deployToRemoteRepo(String version) {
             string(credentialsId: 'GPG_PASSWORD', variable: 'GPG_PASSWORD'), zip(credentialsId: 'gpg_sign_mrchecker', variable: 'GPG_HOMEDIR')
             ]) 
             echo("version: -${version}-");
-            if (version != null || version.isEmpty()){
+            if (version == null || version.isEmpty()){
                 sh"""
                     cd ${env.APP_WORKSPACE}
-                    mvn -Dversion=${version} -DskipTests=true -P release deploy ${env.MVN_PARAMETERS}
+                    mvn -DskipTests=true -P release deploy ${env.MVN_PARAMETERS}
                 """
             } else{
                 sh"""
                     cd ${env.APP_WORKSPACE}
-                    mvn -DskipTests=true -P release deploy ${env.MVN_PARAMETERS}
+                    mvn -Dversion=${version} -DskipTests=true -P release deploy ${env.MVN_PARAMETERS}
                 """
             }
     }	
