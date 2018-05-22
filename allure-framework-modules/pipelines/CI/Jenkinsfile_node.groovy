2f016a9f5e590baf9b3f2f2682c7d5cc11c8a0a4
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2018-05-22 22:58:29 +0200

    Adding variable APP_WORKSPACE

--- allure-framework-modules/pipelines/CI/Jenkinsfile_node.groovy
+++ allure-framework-modules/pipelines/CI/Jenkinsfile_node.groovy
@@ -4,7 +4,7 @@ node(){
 	properties([
 		parameters([
 			APP_WORKSPACE
-			string(defaultValue: '', description: 'Execute job for given Module. Example "allure-framework-modules/allure-security-module/"', name: 'APP_WORKSPACE'),
+			string(defaultValue: '', description: 'Execute job for given Module. Example \"allure-framework-modules/allure-security-module/\"', name: 'APP_WORKSPACE'),
 			string(defaultValue: 'origin/develop', description: 'Execute job on given branch', name: 'WORKING_BRANCH'), 
 			string(defaultValue: '*', description: '''What tests to run
 HelloWorld - run test class -HelloWorld-
