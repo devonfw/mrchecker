6d7c9f5426ccb938cb6319eb79dd46a24a23d0ae
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2018-12-19 17:09:11 +0100

    cucumber runs. Executes Feature files, generates report. Initial issue was with not compiled all Test file

--- mrchecker-app-under-test/src/test/java/com/example/selenium/tests/cucumber/common/stepdefs/MyStepDef.java
+++ mrchecker-app-under-test/src/test/java/com/example/selenium/tests/cucumber/common/stepdefs/MyStepDef.java
@@ -6,7 +6,7 @@ import cucumber.api.java.en.Given;
 
 public class MyStepDef {
 	
-	@Given("I have (\\d+) cukes in my belly")
+	@Given("^I have (\\d+) cukes in my belly$")
 	public void i_have_n_cukes_in_my_belly(int cukes) {
 		BFLogger.logDebug("Cukes: " + cukes);
 	}
