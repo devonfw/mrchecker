40e3b20d360db0908d83af28952e697f69706558
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2018-12-21 15:37:01 +0100

    Prepare to release. Removed SNAPSHOTS

--- mrchecker-app-under-test/pipelines/CI/submodules/BuildCompile.groovy
+++ mrchecker-app-under-test/pipelines/CI/submodules/BuildCompile.groovy
@@ -5,7 +5,7 @@ def call(){
 	stage('Build Compile'){
 		sh"""
             cd ${env.APP_WORKSPACE}
-            mvn -q clean install -DskipTests=true ${env.MVN_PARAMETERS}
+            mvn -q clean ${env.MVN_PARAMETERS}
             mvn -q compile ${env.MVN_PARAMETERS}
             mvn -q test-compile ${env.MVN_PARAMETERS}
         """
