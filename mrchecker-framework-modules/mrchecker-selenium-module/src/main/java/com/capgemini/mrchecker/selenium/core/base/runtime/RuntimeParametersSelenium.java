be4b404a16e12ad8c3cb58b17fa47ad9645e37b0
Lukasz Stefaniszyn <stefaniszynlukasz@gmail.com>
2019-01-17 07:39:58 +0100

    Feature/browser options for grid (#154)
    * update optionBrowsers to support key/value as <String, Object>
    
    * update SeleniumModule to 3.8.2.1

--- mrchecker-framework-modules/mrchecker-selenium-module/src/main/java/com/capgemini/mrchecker/selenium/core/base/runtime/RuntimeParametersSelenium.java
+++ mrchecker-framework-modules/mrchecker-selenium-module/src/main/java/com/capgemini/mrchecker/selenium/core/base/runtime/RuntimeParametersSelenium.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.lang.math.NumberUtils;
+
 import com.capgemini.mrchecker.test.core.base.runtime.RuntimeParametersI;
 import com.capgemini.mrchecker.test.core.logger.BFLogger;
 
@@ -19,15 +22,15 @@ public enum RuntimeParametersSelenium implements RuntimeParametersI {
 	SELENIUM_GRID("seleniumGrid", ""),
 	OS("os", ""),
 	BROWSER_OPTIONS("browserOptions", "") {
-		public Map<String, String> getValues() {
+		public Map<String, Object> getValues() {
 			return Arrays.asList(this.paramValue.split(";"))
 					.stream()
 					.filter(i -> i != "") // remove empty inputs
 					.map(i -> i.split("=", 2)) // split to key, value. Not more than one time
 					.map(i -> new String[] { i[0], (i.length == 1) ? "" : i[1] }) // if value is empty, set empty text
-					.collect(Collectors.toMap(i -> i[0], i -> i[1])); // create Map<String, String>
-			
+					.collect(Collectors.toMap(i -> i[0], i -> (Object) convertToCorrectType(i[1].trim()))); // create
 		}
+		
 	};
 	
 	private String		paramName;
@@ -41,12 +44,35 @@ public enum RuntimeParametersSelenium implements RuntimeParametersI {
 		
 	}
 	
+	protected static Object convertToCorrectType(String value) {
+		Object convertedValue = value;
+		
+		if (null != BooleanUtils.toBooleanObject(value)) {
+			convertedValue = Boolean.valueOf(value);
+			return convertedValue;
+		}
+		
+		if (NumberUtils.isNumber(value)) {
+			if (NumberUtils.isDigits(value)) {
+				// so it is integer value
+				convertedValue = Integer.valueOf(value);
+				return convertedValue;
+			} else {
+				// so it is probably float value
+				convertedValue = Float.valueOf(value);
+				return convertedValue;
+			}
+		}
+		
+		return convertedValue;
+	}
+	
 	@Override
 	public String getValue() {
 		return this.paramValue;
 	}
 	
-	public Map<String, String> getValues() {
+	public Map<String, Object> getValues() {
 		return null;
 	}
 	
--- mrchecker-framework-modules/mrchecker-selenium-module/src/test/java/com/capgemini/mrchecker/selenium/core/base/runtime/RuntimeParametersTest.java
+++ mrchecker-framework-modules/mrchecker-selenium-module/src/test/java/com/capgemini/mrchecker/selenium/core/base/runtime/RuntimeParametersTest.java
@@ -6,14 +6,21 @@ import static org.junit.Assert.assertThat;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.BooleanUtils;
 import org.hamcrest.Matchers;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class RuntimeParametersTest {
 	
 	private Map<String, String> values = new HashMap<String, String>();
+	
+	@Rule
+	public ExpectedException exceptionGrabber = ExpectedException.none();
 	
 	@Before
 	public void setUp() throws Exception {
@@ -22,7 +29,7 @@ public class RuntimeParametersTest {
 		values.put("browserVersion", "11.0");
 		values.put("seleniumGrid", "smth");
 		values.put("os", "linux");
-		values.put("browserOptions", "headless;window-size=1200x600;testEquals=FirstEquals=SecondEquals;--testMe");
+		values.put("browserOptions", "headless;window-size=1200x600;testEquals=FirstEquals=SecondEquals;--testMe;acceptInsecureCerts=true;maxInstances=3");
 		
 		values.forEach(System::setProperty);
 		
@@ -44,27 +51,86 @@ public class RuntimeParametersTest {
 		assertThat("System parameters for empty property 'browserVersion' should be '11.0'", RuntimeParametersSelenium.BROWSER_VERSION.getValue(), Matchers.equalTo("11.0"));
 		assertThat("System parameters for empty property 'seleniumGrid' should be 'smth'", RuntimeParametersSelenium.SELENIUM_GRID.getValue(), Matchers.equalTo("smth"));
 		assertThat("System parameters for empty property 'os' should be 'linux'", RuntimeParametersSelenium.OS.getValue(), Matchers.equalTo("linux"));
-		assertThat("System parameters for empty property 'browserOptions' should be 'headless;window-size=1200x600;testEquals=FirstEquals=SecondEquals;--testMe'",
+		assertThat(
+				"System parameters for empty property 'browserOptions' should be 'headless;window-size=1200x600;testEquals=FirstEquals=SecondEquals;--testMe;acceptInsecureCerts=true;maxInstances=3'",
 				RuntimeParametersSelenium.BROWSER_OPTIONS.getValue(),
-				Matchers.equalTo("headless;window-size=1200x600;testEquals=FirstEquals=SecondEquals;--testMe"));
+				Matchers.equalTo("headless;window-size=1200x600;testEquals=FirstEquals=SecondEquals;--testMe;acceptInsecureCerts=true;maxInstances=3"));
+		
+	}
+	
+	@Test
+	public void testConvertToCorrectTypeBoolean() throws Exception {
+		
+		// validate type as Boolean
+		String value = "true";
+		Object convertToCorrectType = RuntimeParametersSelenium.convertToCorrectType(value);
+		assertThat(BooleanUtils.toBooleanObject((boolean) convertToCorrectType), IsInstanceOf.instanceOf(Boolean.class));
+		
+		value = "false";
+		convertToCorrectType = RuntimeParametersSelenium.convertToCorrectType(value);
+		assertThat(BooleanUtils.toBooleanObject((boolean) convertToCorrectType), IsInstanceOf.instanceOf(Boolean.class));
+		
+		value = "blue";
+		convertToCorrectType = RuntimeParametersSelenium.convertToCorrectType(value);
+		exceptionGrabber.expect(ClassCastException.class);
+		exceptionGrabber.expectMessage("java.lang.String cannot be cast to java.lang.Boolean");
+		assertThat(BooleanUtils.toBooleanObject((boolean) convertToCorrectType), Matchers.not(IsInstanceOf.instanceOf(Boolean.class)));
+		
+	}
+	
+	@Test
+	public void testConvertToCorrectTypeInteger() throws Exception {
+		
+		String value = "1";
+		Object convertToCorrectType = RuntimeParametersSelenium.convertToCorrectType(value);
+		assertThat((Integer) convertToCorrectType, IsInstanceOf.instanceOf(Integer.class));
+		
+		value = "0.23";
+		convertToCorrectType = RuntimeParametersSelenium.convertToCorrectType(value);
+		assertThat((Float) convertToCorrectType, IsInstanceOf.instanceOf(Float.class));
+		
+		value = "blue";
+		convertToCorrectType = RuntimeParametersSelenium.convertToCorrectType(value);
+		exceptionGrabber.expect(ClassCastException.class);
+		exceptionGrabber.expectMessage("java.lang.String cannot be cast to java.lang.Integer");
+		assertThat((Integer) convertToCorrectType, IsInstanceOf.instanceOf(Integer.class));
+	}
+	
+	@Test
+	public void testConvertToCorrectTypeString() throws Exception {
+		
+		String value = "hello";
+		Object convertToCorrectType = RuntimeParametersSelenium.convertToCorrectType(value);
+		assertThat((String) convertToCorrectType, IsInstanceOf.instanceOf(String.class));
+		
+		value = "";
+		convertToCorrectType = RuntimeParametersSelenium.convertToCorrectType(value);
+		assertThat((String) convertToCorrectType, IsInstanceOf.instanceOf(String.class));
+		
+		value = null;
+		convertToCorrectType = RuntimeParametersSelenium.convertToCorrectType(value);
+		assertThat((String) convertToCorrectType, Matchers.isEmptyOrNullString());
 		
 	}
 	
 	@Test
 	public void testBrowserOptionsVariable() throws Exception {
 		
-		Map<String, String> expected = new HashMap();
+		Map<String, Object> expected = new HashMap();
 		expected.put("testEquals", "FirstEquals=SecondEquals");
 		expected.put("headless", "");
-		expected.put("window-size", "1200x600");
 		expected.put("--testMe", "");
+		expected.put("window-size", "1200x600");
+		expected.put("acceptInsecureCerts", true);
+		expected.put("maxInstances", 3);
 		
 		assertThat(RuntimeParametersSelenium.BROWSER_OPTIONS.getValues()
 				.size(),
-				Matchers.is(4));
+				Matchers.is(6));
 		
-		assertThat(RuntimeParametersSelenium.BROWSER_OPTIONS.getValues(),
-				Matchers.is(expected));
+		assertThat(RuntimeParametersSelenium.BROWSER_OPTIONS.getValues()
+				.toString(),
+				Matchers.is(expected.toString()));
 		
 	}
 	
@@ -73,7 +139,8 @@ public class RuntimeParametersTest {
 		
 		RuntimeParametersSelenium.BROWSER_OPTIONS.getValues()
 				.forEach((key, value) -> {
-					String item = (value.isEmpty()) ? key : key + "=" + value;
+					String item = (value.toString()
+							.isEmpty()) ? key : key + "=" + value;
 					System.out.println(item);
 				});
 		
