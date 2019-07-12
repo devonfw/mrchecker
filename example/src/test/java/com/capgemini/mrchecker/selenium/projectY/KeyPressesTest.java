7f3e20b546a9b7e6862e9b1e092c41a50f63b86f
WroMaciej <maciej.a.wroblewski@capgemini.com>
2019-07-12 08:24:52 +0200

    Refactor key test and add enter key validation

--- example/src/test/java/com/capgemini/mrchecker/selenium/projectY/KeyPressesTest.java
+++ example/src/test/java/com/capgemini/mrchecker/selenium/projectY/KeyPressesTest.java
@@ -5,6 +5,7 @@ import com.capgemini.mrchecker.core.groupTestCases.testSuites.tags.TestsFirefox;
 import com.capgemini.mrchecker.core.groupTestCases.testSuites.tags.TestsIE;
 import com.capgemini.mrchecker.core.groupTestCases.testSuites.tags.TestsSelenium;
 import com.capgemini.mrchecker.selenium.pages.projectY.KeyPressesPage;
+import javafx.scene.input.KeyCode;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -12,29 +13,40 @@ import org.junit.experimental.categories.Category;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@Category({ TestsSelenium.class, TestsChrome.class, TestsFirefox.class, TestsIE.class })
+@Category({TestsSelenium.class, TestsChrome.class, TestsFirefox.class, TestsIE.class})
 public class KeyPressesTest extends TheInternetBaseTest {
 
-	private static KeyPressesPage keyPressesPage;
+    private KeyPressesPage keyPressesPage;
 
-	private final String keyToBePressed  = "Q";
-	private final String expectedMessage = "You entered: Q";
+    @Override
+    public void setUp() {
+        keyPressesPage = shouldTheInternetPageBeOpened().clickKeyPressesLink();
 
-	@BeforeClass
-	public static void setUpBeforeClass() {
-		keyPressesPage = shouldTheInternetPageBeOpened().clickKeyPressesLink();
+        logStep("Verify if Key Presses page is opened");
+        assertTrue("Unable to open Key Presses page", keyPressesPage.isLoaded());
+    }
 
-		logStep("Verify if Key Presses page is opened");
-		assertTrue("Unable to open Key Presses page", keyPressesPage.isLoaded());
-	}
+    @Test
+    public void shouldWebsiteReturnInformationAboutPressedKey() {
+        char qKey = 'Q';
+        logStep("Press keyboard Q key");
+        keyPressesPage.pressKey(qKey);
 
-	@Test
-	public void shouldWebsiteReturnInformationAboutPressedKey() {
-		logStep("Press keyboard key");
-		keyPressesPage.pressKey(keyToBePressed);
+        String expectedPageResponse = "You entered: Q";
+        logStep("Verify if website give valid information about pressed keyboard key");
+        assertEquals("Information about pressed key is invalid", expectedPageResponse, keyPressesPage.getPressedKeyInformation());
+    }
 
-		logStep("Verify if website give valid information about pressed keyboard key");
-		assertEquals("Information about pressed key is invalid", expectedMessage, keyPressesPage.getPressedKeyInformation());
-	}
+    @Test
+    public void shouldDetectPressEnter() {
+        char enterKey = '\n';
+        logStep("Press keyboard ENTER key");
+        keyPressesPage.pressKey(enterKey);
+
+        String expectedPageResponse = "You entered: ENTER";
+        logStep("Verify if website give valid information about pressed keyboard key");
+        assertEquals("Information about pressed key is invalid", expectedPageResponse, keyPressesPage.getPressedKeyInformation());
+    }
+
 
 }
