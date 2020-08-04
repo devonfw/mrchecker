e7e55f5d8042788777eb46493c463b01cc3b4b77
Michal Babiarz <michal.babiarz84@gmail.com>
2020-08-04 15:46:22 +0200

    Documentation - updates JUnit5 info step1

--- example/src/test/java/com/capgemini/mrchecker/core/groupTestCases/testSuites/TS_Tag1.java
+++ example/src/test/java/com/capgemini/mrchecker/core/groupTestCases/testSuites/TS_Tag1.java
@@ -1,12 +1,14 @@
 package com.capgemini.mrchecker.core.groupTestCases.testSuites;
 
 import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.ExcludeTags;
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
 @IncludeTags("TestsTag1")
+@ExcludeTags("TagToExclude")
 @SelectPackages("com.capgemini.mrchecker.core.groupTestCases.testCases")
 public class TS_Tag1 {
 	
