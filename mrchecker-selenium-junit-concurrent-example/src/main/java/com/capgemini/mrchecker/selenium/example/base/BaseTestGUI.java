d7ae837c6ee16508141ea403794b1e7b0551ee5c
Rradecki <radoslaw.radecki@capgemini.com>
2023-06-07 15:02:36 +0200

    selenium junit concurrent example - update v2

--- mrchecker-selenium-junit-concurrent-example/src/main/java/com/capgemini/mrchecker/selenium/example/base/BaseTestGUI.java
+++ mrchecker-selenium-junit-concurrent-example/src/main/java/com/capgemini/mrchecker/selenium/example/base/BaseTestGUI.java
@@ -3,44 +3,38 @@ package com.capgemini.mrchecker.selenium.example.base;
 import com.capgemini.mrchecker.test.core.BaseTest;
 import org.opentest4j.TestAbortedException;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public abstract class BaseTestGUI extends BaseTest {
-    private static final Lock setupLock = new ReentrantLock();
-    private static final Lock teardownLock = new ReentrantLock();
-
-    // Each test tear down (post) method
+    //Each test tear down (post) method
     public abstract void tearDownTest();
 
-    // Each test setup (pre) method
+    //Each test setup (pre) method
     public abstract void setUpTest();
 
     @Override
-    // General setup (pre) method used in all tests
-    // Helpful if there is a need to prepare common behavior - for example exceptions handling
-    public final synchronized void setUp() {
+    //General setup (pre) method used in all tests
+    //Helpful if there is a need to prepare common behavior - for example exceptions handling
+    public final void setUp() {
         try {
-            setupLock.lock();
             setUpTest();
         } catch (Throwable throwable) {
-            throw new TestAbortedException("[PRE] " + throwable.getMessage(), throwable);
-        } finally {
-            setupLock.unlock();
+            //Change all fails on pre into Aborted Exception ("skipped" grayed tests on Allure report)
+            //Add [PRE] to the exception message
+            //throw new TestAbortedException("[PRE] " + throwable.getMessage(), throwable);
+            throw throwable;
         }
     }
 
     @Override
-    // General tear down (post) method used in all tests
-    // Helpful if there is a need to prepare common behavior - for example exceptions handling
-    public final synchronized void tearDown() {
+    //General tear down (post) method used in all tests
+    //Helpful if there is a need to prepare common behavior - for example exceptions handling
+    public final void tearDown() {
         try {
-            teardownLock.lock();
             tearDownTest();
         } catch (Throwable throwable) {
-            throw new TestAbortedException("[POST] " + throwable.getMessage(), throwable);
-        } finally {
-            teardownLock.unlock();
+            //Change all fails on post into Aborted Exception ("skipped" grayed tests on Allure report)
+            //Add [POST] to the exception message
+            //throw new TestAbortedException("[POST] " + throwable.getMessage(), throwable);
+            throw throwable;
         }
     }
 }
